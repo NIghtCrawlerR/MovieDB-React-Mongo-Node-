40d63b8b46eeb8f5e2ec7ae948c3dc36623c4718
Angelina <angelina.ratnykova@gmail.com>
2019-05-12 14:01:36 +0300

    fix build

--- backend/server.js
+++ backend/server.js
@@ -22,14 +22,7 @@ connection.once('open', function () {
     console.log("MongoDB successfully connected");
 })
 
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 routes.route('/').get((req, res) => {
 
@@ -95,6 +88,9 @@ routes.route('/delete/:id').delete((req, res) => {
 
 app.use('/movies', routes);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
