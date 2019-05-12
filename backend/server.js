3ae51ab016a1be1e88a02ae9dbac9eb19c533acc
Angelina <angelina.ratnykova@gmail.com>
2019-05-12 13:45:00 +0300

    fix get req

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,17 @@ connection.once('open', function () {
     console.log("MongoDB successfully connected");
 })
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 routes.route('/').get((req, res) => {
 
     Movie.find((err, movies) => {
