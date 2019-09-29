c41f85869cb1162a902b487731772669c0fc123a
Angelina <angelina.ratnykova@gmail.com>
2019-09-29 22:33:03 +0300

    edit genre selector

--- client/src/components/filter.component.jsx
+++ client/src/components/filter.component.jsx
@@ -39,6 +39,7 @@ export default class Filter extends Component {
                     <div className="row">
                         <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12 my-1">
                             <select className="form-control" name="genre">
+                                <option value="">All genres</option>
                                 {genres.map((genre, i) => {
                                     return <option value={genre} key={i}>{genre}</option>
                                 })}
@@ -51,8 +52,8 @@ export default class Filter extends Component {
                                         <div className="col-sm-6 col-xs-6 my-1">
                                             <select name="watched" className="form-control">
                                                 <option value="">All (watched/not watched)</option>
-                                                <option value="1">Watched</option>
-                                                <option value="0">Not watched</option>
+                                                <option value={true}>Watched</option>
+                                                <option value={false}>Not watched</option>
                                             </select>
                                         </div>
                                         <div className="col-sm-6 col-xs-6 my-1">
